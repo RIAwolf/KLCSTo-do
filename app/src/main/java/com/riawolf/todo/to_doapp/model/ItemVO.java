b6d5dc0a1446a4a6bfec07cdaa597635bf7ebba2
Martynas <packmaster@riawolf.com>
2018-02-03 15:53:22 +0200

    pirmas ikelimas

---
+++ app/src/main/java/com/riawolf/todo/to_doapp/model/ItemVO.java
@@ -0,0 +1,11 @@
+package com.riawolf.todo.to_doapp.model;
+
+/**
+ * Created by user on 2/3/2018.
+ */
+
+public class ItemVO {
+    public int id;
+    public int done;
+    public String title;
+}
